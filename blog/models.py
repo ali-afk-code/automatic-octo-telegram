f2824eabac929daf72b9aa8c97e471c89af6f6e4
Muhammadali <ifatcat1111@gmail.com>
2020-07-22 17:17:45 +0500

    forms and user accounts

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse 
 class Post(models.Model):
     title = models.CharField(max_length =200 )
     author = models.ForeignKey(
@@ -6,5 +7,8 @@ class Post(models.Model):
                                 on_delete= models.CASCADE,
                                 )
     body = models.TextField()
-    def __str__ (self ):
+    
+    def __str__ (self):
         return self.title
+    def get_absolute_url(self): # new
+        return reverse('post_detail' , args =[str(self.id)])
